ccc4babe9b3714978221a02f4022932e3fac790b
BAGEORG <basil.george@daimler.com>
2020-04-02 15:04:38 +0530

    Add project files.

---
+++ UnitTestForms/ViewModels/AboutViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Input;
+
+using Xamarin.Forms;
+
+namespace UnitTestForms.ViewModels
+{
+    public class AboutViewModel : BaseViewModel
+    {
+
+        public bool TestMe { get; set; }
+        public  String TestMeString { get; set; }
+        public AboutViewModel()
+        {
+            Title = "About";
+            TestMeString = "Try";
+
+            OpenWebCommand = new Command(() =>
+            {
+                TestMe = true;
+
+           //     Device.OpenUri(new Uri("https://xamarin.com/platform"));
+            });
+
+            TestMeFalseCommand = new Command(() =>
+            {
+                TestMeString = "Try this";
+            });
+        }
+
+        public ICommand OpenWebCommand { get; }
+
+        public ICommand TestMeFalseCommand { get; }
+    }
+}
